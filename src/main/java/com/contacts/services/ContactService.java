a90c0dcf59e04f3f75e9cac8fa62f8f84f93e5b7
Daniel Villarreal <cdvillarreal@gmail.com>
2022-07-16 19:10:45 -0300

    Api rest y mvc terminados

--- src/main/java/com/contacts/services/ContactService.java
+++ src/main/java/com/contacts/services/ContactService.java
@@ -1,5 +1,6 @@
 package com.contacts.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,29 +8,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.contacts.interfaceServices.ContactInterfaceService;
-import com.contacts.interfaces.ContactInterface;
 import com.contacts.models.ContactModel;
+import com.contacts.repositories.ContactRepository;
 
 @Service
 public class ContactService implements ContactInterfaceService{
 
 	@Autowired
-	private ContactInterface data;
+	private ContactRepository contactRepository;
+	
+	public ArrayList<ContactModel> getAllContacts() {
+		return (ArrayList<ContactModel>) contactRepository.findAll();
+	}
+	
+	public ContactModel saveContact(ContactModel contact) {
+		return contactRepository.save(contact);
+	}
+	
+	public Optional<ContactModel> getById(Integer id) {
+		return contactRepository.findById(id);
+	}
+	
+	public ArrayList<ContactModel> getByName(String name) {
+		return contactRepository.findByName(name);
+	}
+	
+	public boolean deleteContact(Integer id) {
+		try {
+			contactRepository.deleteById(id);
+			return true;
+		} catch(Exception error) {
+			return false;
+		}
+	}
 	
 	@Override
 	public List<ContactModel> listing() {
-		return (List<ContactModel>)data.findAll();
+		return (List<ContactModel>)contactRepository.findAll();
 	}
 
 	@Override
 	public Optional<ContactModel> listingId(int id) {
-		return data.findById(id);
+		return contactRepository.findById(id);
 	}
 
 	@Override
 	public int save(ContactModel c) {
 		int res = 0;
-		ContactModel contact = data.save(c);
+		ContactModel contact = contactRepository.save(c);
 		if(!contact.equals(null)) {
 			res = 1;
 		}
@@ -38,6 +64,6 @@ public class ContactService implements ContactInterfaceService{
 
 	@Override
 	public void delete(int id) {
-		data.deleteById(id);
+		contactRepository.deleteById(id);
 	}
 }
